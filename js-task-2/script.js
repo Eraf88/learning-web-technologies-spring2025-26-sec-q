869cb66aef14bc389dd816f78511aa67a5235c6e
Eraf88 <erafaman9@gmail.com>
2026-03-11 13:29:34 +0600

    final (incomplete)

--- js-task-2/script.js
+++ js-task-2/script.js
@@ -15,3 +15,37 @@ let winPatterns = [
 [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 ];
 
+for(let i =0; i<cells.length; i++){
+    cells[i].addEventListener('click',handleClick);
+}
+
+resetBtn.addEventListener('click',resetGame);
+
+function handleClick(){
+    let index = this.getAttribute('data-index');
+
+    if (board[index] != "" || gameActive == false) {
+        return;
+    }
+
+    board[index] = currentPlayer;
+    this.textContent = currentPlayer;
+    this.classList.add('marked');
+
+    checkWinner();
+}
+
+function checkWinner(){
+    for(let i=0; i<winPatterns.length; i++){
+        let a = winPatterns[i][0];
+        let b = winPatterns[i][1];
+        let c = winPatterns[i][2];
+
+        if (board[a] != "" && board[a] == board[b] && board[a] == board [c]) {
+            cells[a].classList.add("winner");
+            cells[b].classList.add("winner");
+            cells[c].classList.add("winner");
+        }
+
+    }
+}
